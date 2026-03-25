da23cc563be6cbeeac214a05519f77c6c2f0d9fa
Matt <matt@Mac.lan>
2026-03-24 23:08:22 -0500

    Fixing Bugs For Run

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
 import qunum.numerical as qn
+from qunum.jupyter_tools.plotting import PlotIt
 from qunum.numerical.physics.quantum.heisenberg.bbgky_truncation.bbgky import BBGKYDecoupling as BBGKY
 import os, torch
 import time
+import tqdm
+from plotly import graph_objects as go 
+import plotly.io as pio
+
+
 def mk_obj(N:int, n:int,**kwargs)->BBGKY:
-    a  = dict(
-        adaptive_time_step= True, 
-        tgt_u0 = 5,
-        u0 = 10_000*1e2, 
-        tgt_u0 = 5,
-        debug=False, load_data=False, dp = .1, 
-        p0 = torch.tensor(1e10), 
-        method = 'fullrk', 
-        adaptive_epsilon=5e-3, 
-        dt  = 1e-2, 
-        adaptive_time_step=True, 
-        r0_Rv = 7.5, 
-        p_start_f = .1,
-        angular_dependnce = False
-    )
+    print(N,n)
+    a = dict(Nsteps= int(4500), u0 = 10_000*1e2, tgt_u0 = 5, debug=False, load_data=False, dp = .1, p0 = torch.tensor(1e10), method = 'gaussrk', adaptive_epsilon=5e-3, dt  = 1e-2, adaptive_time_step=True, r0_Rv = 7.5, p_start_f = .1)
     a.update(kwargs)
     return BBGKY(
-        N, n, 
-        data_storage_loc=os.path.join(os.getcwd(), 'BBGKYSims', f'BBGKY_{N}-{n}'),
+        N=N, n=n, 
+        data_storage_loc=os.path.join('BBGKYSims', f'BBGKY_{N}-{n}'),
         **a
     )
 if __name__ == '__main__':
-    
     D = {2:range(50, 105, 5), 3:range(25,75,5), 4:range(10,50,5)}
-    
-    for n in D:
-        for N in D:
-            Obj = mk_obj(N, n)
+    O = mk_obj(10,2, Nsteps = 5_000, renorm = True)
+    for o in tqdm.tqdm(O):
+        pass
+    plt = PlotIt()
+    plt.extend_data(*(dict(x = O.dt.t[:-1], y = O.oneBodyRenyiEntropy(A = A)[:,0]) for A in range(O.N)))
+    plt.show()
+    for n, rng in zip(D.keys(), D.values()):
+        for N in rng:
+            O = mk_obj(N, n)
             t0 = time.time()
-            for o in Obj:
+            print(O)
+            for o in tqdm.tqdm(O):
                 pass
             dt = time.time() - t0
             with open(os.path.join(os.getcwd(), 'log.txt'), 'a') as file:
-                file.write("Time for (N = {N},n = {n}): {dt:.2e} s\n".format(N = str(N), n = str(n), dt = dt))
+                file.write("Time for {BB}: {dt:.2e} s\n".format(BB=str(O), dt = dt))
     pass
