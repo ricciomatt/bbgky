1b4c888b4415af10443bc86f5621e2d88bd39d31
Matt <matt@Mac.lan>
2026-03-26 23:01:33 -0500

    fixing stuff

--- multianlge.py
+++ multianlge.py
@@ -12,15 +12,28 @@ import numpy as np
 def mk_obj(N:int, n:int,**kwargs)->BBGKY:
     a = dict(Nsteps= int(4500), u0 = 10_000*1e2, tgt_u0 = 5, debug=False, load_data=False, dp = .1, p0 = torch.tensor(1e10), method = 'fullrk', adaptive_epsilon=5e-3, dt  = 1e-2, adaptive_time_step=True, r0_Rv = 7.5, p_start_f = .1)
     a.update(kwargs)
+    i = 0
+    while True:
+        path = os.path.join('BBGKYMuliAngleSims', f'BBGKY_{N}-{n}_{i}')
+        if(os.path.exists(path)):
+            break
+        i+=1
     return BBGKY(
         N=N, n=n, 
-        data_storage_loc=os.path.join('BBGKYMuliAngleSims', f'BBGKY_{N}-{n}'), angular_dependence='random_uniform',
+        data_storage_loc=os.path.join('BBGKYMuliAngleSims', f'BBGKY_{N}-{n}_{1}'), angular_dependence='random_uniform',
         timeit=True,
         **a
     )
 if __name__ == '__main__':
-    n = int(sys.argv[1])
-    N = int(sys.argv[2])
+    try:
+        n = int(sys.argv[1])
+    except:
+        n = 2
+    try:
+        N = int(sys.argv[2])
+    except: 
+        N = 100
+    
     tc = time.time()
     O = mk_obj(N, n)
     t0 = time.time()
