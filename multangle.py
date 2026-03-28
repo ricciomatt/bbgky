1632f0452f05c2143add57643c020ebf8b466b1d
Matt <matt@Mac.lan>
2026-03-28 13:31:03 -0500

    Fixing Machine Issues

--- multangle.py
+++ multangle.py
@@ -9,19 +9,23 @@ if __name__ == '__main__':
         key, val = arg.split('=')
         kwargs[key] = int(val)
     tc = time.time()
-    if not os.path.exists(os.path.join(os.getcwd(), f'BBGKY{kwargs['N']}-{kwargs['n']}/indexmap')):
-        CMap(kwargs['N'], kwargs['n'], maps_loc=f'BBGKY{kwargs['N']}-{kwargs['n']}/indexmap')
-    O = mk_obj(map_loc = f'BBGKY{kwargs['N']}-{kwargs['n']}/indexmap', **kwargs)
+    N = kwargs["N"]
+    n = kwargs['n']
+    if not os.path.exists(os.path.join(os.getcwd(), f"BBGKY{N}-{n}/indexmap")):
+        CMap(N=N, n= n, maps_loc=f"BBGKY{N}-{N}/indexmap")
+    O = mk_obj(map_loc = f'BBGKY{N}-{n}/indexmap', **kwargs)
     t0 = time.time()
     print('Creation time {dt:.2e}s'.format(dt =  t0-tc))
+    status = 'Completed'
     for i, o in tqdm.tqdm(enumerate(O)):
         if(o.isnan().any()):
             print(i, o)
+            status = 'Failed'
             break
         pass
     dt = time.time() - t0
     with open(os.path.join(O.data_loc, f'log_{i}.txt'), 'w') as file:
-        file.write("Nsteps {n} computed in {dt:.2e}\nFOR {BB}\nCreation Time {dtc}".format(n = O.n, BB=str(O), dt = dt, dtc = tc - t0))
+        file.write("Status:{status}\nNsteps Taken = {n} computed in {dt:.2e}\nFOR {BB}\nCreation Time {dtc}".format(status = status, n = O.n, BB=str(O), dt = dt, dtc = tc - t0))
     try:
         with open(os.path.join(O.data_loc, 'times.dat', 'w')) as file:
             np.array(O.delta_t, dtype = np.float64).tofile(file)
