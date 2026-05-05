4543a3d3572888121d520df3b4e252d8094dc1db
Matt Riccio <riccio_matt@outlook.com>
2026-05-05 11:40:28 -0500

    Plot Update

--- core/plot.py
+++ core/plot.py
@@ -1,5 +1,5 @@
 import copy
-from qunum.jupyter_tools.plotting import PlotIt
+from qunum.jupyter_tools.plotting.engine import PlotIt, plotly_configs
 from qunum.numerical import SUConnection, AdaptiveLinspace
 from qunum.numerical import LazyTensor
 from qunum.numerical.physics.quantum.heisenberg.bbgky_truncation.core.core import rebuild_timeDep
@@ -12,13 +12,14 @@ from math import sqrt as msqrt
 from qunum.numerical.physics.quantum.operators.dense.nuetrino import pmns2, pmns3
 from qunum.numerical.physics.quantum.heisenberg.bbgky_truncation.core.core import pmnsrotPhi
 
+
 def plot_non_local(
-        PhiBar:torch.Tensor, GammaBar:torch.Tensor, t:torch.Tensor, pMag:torch.Tensor, 
-        plot_path:str, sun:SUConnection, lfs:dict[str:pl.LazyFrame], plt:PlotIt, to_image:bool = True,
-    )->None:
+    PhiBar:torch.Tensor, GammaBar:torch.Tensor, t:torch.Tensor, pMag:torch.Tensor, 
+    plot_path:str, sun:SUConnection, lfs:dict[str:pl.LazyFrame], plt:PlotIt, to_image:bool = True,
+)->None|PlotIt:
     NSamp = 200
     N = 100
-    lf:pl.LazyFrame = lfs['lf']
+    lf:pl.LazyFrame = lfs['lf'].clone()
     IXS = lf.select(
         'A','B','C'
     ).explode(
@@ -36,29 +37,30 @@ def plot_non_local(
     ).explode(
         'C'
     ).collect().sample(NSamp).sort('A','B','C')
-
     IXS = IXS.to_numpy()
     tI, Mp = mutualInformation(PhiBar, GammaBar, sun =sun, lfs = lfs, ret_map= True)
     I = torch.zeros((tI.shape[0], PhiBar.shape[1], PhiBar.shape[1]), dtype = tI.dtype)
     I[:, Mp['Aix'], Mp['Bix']] = tI[:, Mp['ABix']]
-
+    print('I2')
     S2, Mp2 = twoBodyRenyiEntropy(PhiBar, GammaBar,ret_map=True, retfull=True)
-    print(Mp)
+   
     S = torch.zeros((tI.shape[0], PhiBar.shape[1], PhiBar.shape[1]), dtype = tI.dtype)
     S[:, Mp['Aix'], Mp['Bix']] = S2[:, Mp['AB']]
+    print('S2')
     I3 = list(
         (
             twoBodyRenyiEntropy(PhiBar, GammaBar, A = np.sort(ix[:2]), B = [ix[2]]).sum(-1) - oneBodyRenyiEntropy(PhiBar, A = ix[2]).flatten() - threeBodyRenyiEntropy(PhiBar, GammaBar,A = ix[0], B=ix[1], C=ix[2] ).flatten()
             for ix in IXS
         )
-    )
+    ) 
+    print('I3')
 
 
     #3-Body Renyi
     S3 = list(threeBodyRenyiEntropy(PhiBar, GammaBar, sun, lfs, A = ix[0], B = ix[1], C=ix[2]).real for ix in IXS)
     S3Bar = sum(S3)/NSamp
 
-
+    print('S3')
 
     print('Computed')
 
@@ -171,33 +173,69 @@ def plot_non_local(
         xaxis = 'x4', yaxis = 'y4', legend='legend4'
     )
 
-
+    ax = plotly_configs()['defaults']['axis']
+    
+    xaxis5 = copy.deepcopy(ax)
+    xaxis5['domain'] =  (.575, 1)
+    xaxis5['title'] = '$\\huge t(\\omega_{0}^{-1})$'
+    xaxis5['anchor'] = 'y5'
+    
+    xaxis6 = copy.deepcopy(ax)
+    xaxis6['domain'] =  (.575, 1)
+    xaxis6['domain'] =  (.575, 1)
+    xaxis6['title'] = r'$\huge \frac{p_{A}}{E_{0}}$'
+    xaxis6['anchor'] = 'y6'
+    
+    yaxis5 = copy.deepcopy(ax)
+    yaxis5['domain'] =  copy.copy(plt.layout['yaxis']['domain'])
+    yaxis5['title'] = r'$\huge {S}_{2,(ABC)} $'
+    yaxis5['anchor'] = 'x5'
+    
+    yaxis6 = copy.deepcopy(ax)
+    yaxis6['domain'] =  copy.copy(plt.layout['yaxis2']['domain'])
+    yaxis6['title'] = r'$\huge \frac{p_{A}}{E_{0}}$'
+    yaxis6['anchor'] = 'x6'
+    
     plt.update_layout(
         title =dict(text = 'Non Locality Indicators', font = dict(size = 40), x = 0), 
         xaxis = dict(title = '$\\huge t(\\omega_{0}^{-1})$', domain = (0,.425)),
         xaxis2 = dict(title = r'$\huge \frac{p_{A}}{E_{0}}$', domain = (0,.425) ),
         xaxis3 = dict(title = '$\\huge t(\\omega_{0}^{-1})$'), 
         xaxis4 = dict(title = '$\\huge t(\\omega_{0}^{-1})$'), 
-        xaxis5 = dict(title = '$\\huge t(\\omega_{0}^{-1})$', domain = (.575, 1), anchor = 'y5'), 
-        xaxis6 = dict(title = r'$\huge \frac{p_{A}}{E_{0}}$', domain = (.575, 1), anchor = 'y6'),
+        xaxis5 = xaxis5, 
+        xaxis6 = xaxis6,
 
         yaxis = dict(title = '$\\huge \\mathcal{I}_{2,(AB)}$'),
         yaxis2=dict(title = r"$\huge \frac{p_{A}}{E_{0}}$"),
         yaxis3 = dict(title = r'$\huge S_{2,(AB)} $'),
         yaxis4 = dict(title = r'$\huge \mathcal{I}_{2,(AB:C)} $'),
-        yaxis5 = dict(title = r'$\huge {S}_{2,(ABC)} $', domain = copy.copy(plt.layout['yaxis']['domain']), anchor = 'x5'),
-        yaxis6 = dict(title = r'$\huge \frac{p_{A}}{E_{0}}$', domain = copy.copy(plt.layout['yaxis2']['domain']), anchor = 'x6'),
+        yaxis5 = yaxis5,
+        yaxis6 = yaxis6,
+
+        
 
         height = 3500
 
     )
+    legends = dict(
+        legend =  dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+        legend2 = dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+        legend3 = dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+        legend4 = dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+        legend5 = dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+        legend6 = dict(yanchor = 'top',bordercolor="black", borderwidth=3, title = dict(text = r"", font = dict(size = 28)), itemwidth = 50),
+    )
+    for tit in set({'', '2','3','4','5','6'}):
+        legends[f'legend{tit}']['y'] = plt.layout[f'yaxis{tit}']['domain'][1]
+        legends[f'legend{tit}']['x'] = plt.layout[f'xaxis{tit}']['domain'][1]
+    plt.update_layout(**legends)
     titles = {
         '6':r'$\huge\text{{Two  Body  Entropy  Heat  Map}}(t(\omega_{{0}}^{{-1}})={t})$'.format(t = int(t[i])), 
         '4':r'$\huge\text{Three Body Entropy}$', 
         '3':r'$\huge\text{Three Body Mutual Information Estimate}$', 
         '5':r'$\huge\text{Two Body Entropy}$',
         '2':r'$\huge\text{{Two  Body Mutual  Information  Heat  Map}}(t(\omega_{{0}}^{{-1}})={t})$'.format(t = int(t[i])),
-        '':r'$\huge\text{Two Body Mutual Information Estimate}$'.format(t[i]),
+        '':r'$\huge\text{Two Body Mutual Information Estimate}$'
     }
     annotations = [
         dict(
@@ -248,7 +286,7 @@ def plot_ptrans(
         PhiBar:torch.Tensor, tfact:float, t:torch.Tensor, 
         plot_path:str, sun:SUConnection, B:LazyTensor,
         plt:PlotIt, u:LazyTensor, N:int, to_image:bool = True
-    )->None:
+    )->None|PlotIt:
     
     plt.reset()
     plt.subplot_grid(ncols=1, nrows=4, spacingr=.05, leg_opts=dict(bordercolor="black", borderwidth=3))
@@ -260,19 +298,19 @@ def plot_ptrans(
         name = '$\\huge \\bar{\\Phi}_a\\bar{\\Phi}^a$', legendgroup = 'A', showlegend = True, 
     )
 
-
-    plt.add_data(
-        x = t, y = u(tfact*t).real, 
-        yaxis= 'y4', xaxis = 'x4', legend = 'legend4',
-        line=dict(width = 4), marker = dict(color = 'purple'), 
-        name = '$\\huge \\mu_{(t)}$', showlegend = True
-    )
     plt.add_data(
         x = [0, t[-1]], y = [B(0)[:,2].real.mean().item(), B(0)[:,2].real.mean().item()],
         line=dict(width = 4, dash='dash'), marker=dict(color = 'orange'),   mode='lines',
         yaxis = 'y4', xaxis ='x4', legend = 'legend4', 
         name = '$\\huge|\\bar{B}|$', showlegend = True
     )
+    plt.add_data(
+        x = t, y = u(tfact*t).real, 
+        yaxis= 'y4', xaxis = 'x4', legend = 'legend4',
+        line=dict(width = 4), marker = dict(color = 'purple'), 
+        name = '$\\huge \\mu_{(t)}$', showlegend = True
+    )
+   
     S = oneBodyRenyiEntropy(PhiBar, sun = sun, )
 
     plt.extend_data(*(
@@ -313,10 +351,10 @@ def plot_ptrans(
         showlegend = True, name = '$\\huge \\bar{\\mathcal{M}}_{4}$', legendgroup = 'M'
     )
     titles = {
-        '4':r'$\huge\text{Energy Scales}$', 
-        '3':r'$\huge\text{Order Parameter}$', 
-        '2':r'$\huge\text{Entropy Evolution}$',
-        '':r'$\huge\text{Magic Evolution}$',
+        '4':r'$\huge\textbf{Energy Scales}$', 
+        '3':r'$\huge\textbf{Order Parameter}$', 
+        '2':r'$\huge\textbf{Entropy Evolution}$',
+        '':r'$\huge\textbf{Magic Evolution}$',
     } 
     annotations = [
         dict(
@@ -346,23 +384,23 @@ def plot_ptrans(
     plt.update_layout(
         height = 2500, width = 2000, 
         title = dict(
-            text = r'$\huge \textbf{Dynamical Phase Transition  around  the  effective Daul Point}(\mu_{(t)} \sim |\bar{B}|)$',
+            text = r'$\Huge \textbf{Dynamical  Phase  Transition  around  the  effective  Daul  Point}(\mu_{(t)} \sim |\bar{B}|)$',
             yref= 'paper', x=0, xref = 'paper'
         ),
         yaxis = dict(title = dict(text = r"$\huge \mathcal{M}_{4,A}$")),
         yaxis2= dict(title = dict(text = r"$\huge S_{2,A}$")),
         yaxis3= dict(title = dict(text = r"$\huge \bar{\Phi}_{a}\bar{\Phi}^{a}$")),
-        yaxis4= dict(title = dict(text = r"$\huge \text{Energy Scale}s$")),
+        yaxis4= dict(title = dict(text = r"$\huge E$")),
 
         xaxis = dict(title = dict(text = r"$\huge t(\omega_{0}^{-1})$")),
         xaxis2= dict(title = dict(text = r"$\huge t(\omega_{0}^{-1})$")),
         xaxis3= dict(title = dict(text = r"$\huge t(\omega_{0}^{-1})$")),
         xaxis4= dict(title = dict(text = r"$\huge t(\omega_{0}^{-1})$")),
         
-        legend = dict(title = dict(text = r"One-Body Magic"), font = dict(size = 35), xanchor = 'right'),
-        legend2 = dict(title = dict(text = r"One-Body Entropy"), font = dict(size = 35), xanchor = 'right'),
-        legend3 = dict(title = dict(text = r"Order Paramter"), font = dict(size = 35), xanchor = 'right'),
-        legend4 = dict(title = dict(text = r"Energy Scales"), font = dict(size = 35), xanchor = 'right'),
+        legend = dict(title = dict(text = r"One-Body Magic"), font = dict(size = 28), xanchor = 'left', itemwidth=85),
+        legend2 = dict(title = dict(text = r"One-Body Entropy"), font = dict(size = 28), xanchor = 'left', itemwidth=85),
+        legend3 = dict(title = dict(text = r"Order Paramter"), font = dict(size = 28), xanchor = 'left', itemwidth=85),
+        legend4 = dict(title = dict(text = r'Energy Scales'), font = dict(size = 28), xanchor = 'left', itemwidth=85),
 
         shapes = shapes,
         annotations = annotations
@@ -376,8 +414,9 @@ def plot_ptrans(
     else:
         return plt
 
-
-def plot_flav(PhiBar:torch.Tensor, sun:SUConnection, plt:PlotIt, t:torch.Tensor , path_base:str, N:int, to_image:bool = True)->None:
+def plot_flav(
+        PhiBar:torch.Tensor, sun:SUConnection, plt:PlotIt, t:torch.Tensor , path_base:str, N:int, to_image:bool = True
+    )->None|PlotIt:
     Pz = pmnsrotPhi(PhiBar, sun)[...,]
     plt.reset()
     color = ['rgba(255,0,0,.5)', 'rgba(0,0,255,.5)']
@@ -389,13 +428,13 @@ def plot_flav(PhiBar:torch.Tensor, sun:SUConnection, plt:PlotIt, t:torch.Tensor 
                 x = t, y = Pz[...,i,k].real, 
                 marker=dict(color = color[m]), line = dict(width = 5), 
                 showlegend = bool(i<1), 
-                name = f'$\\huge \\Phi_{{A{str(int(k))}}}$'
+                name = r'$\huge n_{A\nu_{e}}-n_{A\nu_{x}}$'
             ) for i in range(N)
         ))
         plt.add_data(
             x = t, y = Pz[...,k].real.mean(-1), 
             marker=dict(color = color_Bar[m]), line = dict(width = 10, dash = dash[m]), 
-            showlegend = True, name = f'$\\huge \\bar{{\\Phi}}_{str(int(k))}$'
+            showlegend = True, name = r'$\huge\langle n_{\nu_{e}}-n_{\nu_{x}}\rangle$'
         )
     plt.update_layout(
         xaxis = dict(title = dict(text = r'$\huge {t(\omega_{0}^{-1})}$')),
@@ -407,3 +446,4 @@ def plot_flav(PhiBar:torch.Tensor, sun:SUConnection, plt:PlotIt, t:torch.Tensor 
         return 
     else:
         return plt
+        
