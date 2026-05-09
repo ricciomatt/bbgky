ae64c6be7a30383617b41bb82a7f9f69c459e6f5
Matt Riccio <riccio_matt@outlook.com>
2026-05-09 08:30:30 -0500

    Run Accum Fix

--- run_accumulate.py
+++ run_accumulate.py
@@ -1,10 +1,12 @@
 import sys
 from core.core import run_job, unpack_script_args, run_job_cums
-
+from qunum.numerical.physics.quantum.heisenberg.bbgky_truncation.core.core import fit_r0
 if __name__ == '__main__':
-    kwargs = dict(n = 2, N = 100, Nsteps = 5000, Nproc = 5, tot_proc = 100, renorm = False, max_cores = 32, pycmd='python3')
+    kwargs = dict(n = 2, N = 100, Nsteps = 5000, Nproc = 5, u0 = 10_000*1e2, tgt_u0 = 5, tot_proc = 100, renorm = False, max_cores = 32, pycmd='python3')
     print(sys.argv)
     kwargs= unpack_script_args(sys.argv, **kwargs)
+    kwargs['r0_Rv'] = fit_r0(tgtu0=kwargs['tgt_u0'],u0=kwargs['u0'], N = kwargs['N'])
     print(kwargs)
+    print('init_jobs')
     run_job_cums(**kwargs)
     print('Done')
